cc1c7b8bd4ca1a520efaa54585305c3cfb4323fa
Yuto Terada <lugongdao07@gmail.com>
2025-02-15 15:01:46 +0900

    add: ユーザー登録エンドポイントを実装し、Prismaを使用してデータを保存

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,53 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { zValidator } from '@hono/zod-validator'
+import type { Bindings } from "./bindings";
+import { createPrismaClient } from "./prisma";
+import { registerSchema } from "./schema";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Bindings }>();
+app.post("/register", zValidator("json", registerSchema), async (c) => {
+    const prisma = createPrismaClient(c);
+    const body = c.req.valid("json");
+
+    const user = await prisma.user.create({
+        data: {
+            name: body.userData.name,
+            nameKana: body.userData.nameKana,
+            cpuSerialNumber: body.deviceData.cpuSerialNumber,
+        },
+    });
+    await prisma.userBodyData.create({
+        data: {
+            userId: user.id,
+            weight: body.userData.bodyData.weight,
+            height: body.userData.bodyData.height,
+            bodyFatPercentage: body.userData.bodyData.bodyFatPercentage,
+            gender: body.userData.bodyData.gender !== 0 ? "MALE" : "FEMALE"
+        },
+    });
+    await prisma.userLifecycle.create({
+        data: {
+            userId: user.id,
+            wakeUpTime: body.userData.lifeCycle.wakeUpTime,
+            sleepTime: body.userData.lifeCycle.sleepTime,
+        },
+    });
+    await prisma.userLikeFood.createMany({
+        data: body.userData.likes.likeFoods.map((food) => ({
+            userId: user.id,
+            food: food,
+        })),
+    });
+    await prisma.userLikeHobby.createMany({
+        data: body.userData.likes.likeHobbies.map((hobby) => ({
+            userId: user.id,
+            hobby: hobby,
+        })),
+    });
+
+    return c.json({
+        userId: user.id
+    });
+});
+
+export default app;
