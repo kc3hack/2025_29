ca6a607ab6a704a05dfc42fe8d539e6c52776fdb
Yuto Terada <lugongdao07@gmail.com>
2025-02-21 22:15:07 +0900

    feat: デモ用にユーザーIDを固定で返すように認証処理を変更

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -2,10 +2,13 @@ import type { Context } from "hono";
 import { verify } from "hono/jwt";
 
 export async function auth(c: Context<{ Bindings: Env }>) {
-    const header = c.req.header("Authorization");
-    if (!header) return null;
-    if (!header.startsWith("Bearer ")) return null;
-    const token = header.split(" ")[1];
-    const payload = await verify(token, c.env.JWT_SECRET as string);
-    return payload.uid as string;
+    // const header = c.req.header("Authorization");
+    // if (!header) return null;
+    // if (!header.startsWith("Bearer ")) return null;
+    // const token = header.split(" ")[1];
+    // const payload = await verify(token, c.env.JWT_SECRET as string);
+    // return payload.uid as string;
+
+    // デモの時はユーザーIDを固定で返す
+    return "a1225c7b-851c-4861-8b3e-2900091a2084";
 }
